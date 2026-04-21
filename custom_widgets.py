bcb8700414511182c6a858c74665e5765b28fc0b
Chad McDaniel <christopher.c.mcdaniel@usm.edu>
2026-04-21 16:12:38 -0500

    Added full solution for the front-end.

--- custom_widgets.py
+++ custom_widgets.py
@@ -15,5 +15,24 @@ class TabButton(Button):
         app = App.get_running_app()
         app.shell.change_screen(self.route)
 
+class TextBox(BoxLayout):
+    text = StringProperty("")
+    hint_text = StringProperty("")
+    is_multiline = BooleanProperty(False)
+    back_color = ColorProperty(get_color_from_hex("#D9D9D9"))
+
+class ArrowButton(Button):
+    is_left = BooleanProperty(True)
+
+class ContentBox(BoxLayout):
+    title_text = StringProperty()
+
+class CheckmarkBox(CheckBox):
+    background_color = ColorProperty(get_color_from_hex("#000000"))
+    checkmark_name = StringProperty("check-solid-dark.png")
+
+class AppButton(Button):
+    color_normal = ColorProperty(get_color_from_hex("#87E5FF"))
+    color_pressed = ColorProperty(get_color_from_hex("#126f89"))
 
 Builder.load_file('custom_widgets.kv')
